643d64c20443980142d89330541646c36be6b888
Jabez Rafael Abella <jabezrafael1@gmail.com>
2025-05-17 21:49:32 +0800

    Add LoadingScreen widget with customizable size and color for loading animations

---
+++ lib/widgets/loading_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class LoadingScreen extends StatelessWidget {
+  final double size;
+  final Color color;
+  const LoadingScreen({
+    super.key,
+    this.size = 50.0,
+    this.color = Colors.yellow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LoadingAnimationWidget.fourRotatingDots(
+          color: color,
+          size: size,
+        ),
+      ),
+    );
+  }
+}
